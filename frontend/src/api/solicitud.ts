3a7f6ea2f0f78e14e832da25ce6461063d2f48bf
Franquinios <francopietrantuono999@gmail.com>
2025-08-01 01:30:31 -0300

    feat: add CrearSolicitud page

--- frontend/src/api/solicitud.ts
+++ frontend/src/api/solicitud.ts
@@ -1,3 +1,4 @@
+import type Usuario from "../interfaces/UsuarioInterface";
 import api from "./api";
 
 const entity: string = "solicitudes";
@@ -6,8 +7,18 @@ export const getAllSolicitudes = () => api.get(entity).then(({ data }) => data);
 
 export const getSolicitudById = (id: number) => api.get(`${entity}/${id}`).then(({ data }) => data);
 
-export const createSolicitud = ({ ...solicitud }) => {
-    api.post(entity, solicitud).then(({ data }) => data);
+export const createSolicitud = async (tipo: string, nombre: string, referencia: string, mensaje: string, pais: string | null = null, provincia: string | null = null, usuario: Usuario) => {
+  if (pais === '') pais = null;
+  if (provincia === '') provincia = null;
+  
+  const response = await api.post(entity, { tipo, nombre, referencia, mensaje, pais, provincia, usuario }, {
+    withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.status === 201) return response.data  
 }
 
 export const deleteSolicitud = (id: string) => api.delete(`${entity}/${id}`);

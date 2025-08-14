f9ad992d8c22a6625d403b51092fc533e8afd548
Franquinios <francopietrantuono999@gmail.com>
2025-08-14 03:29:04 -0300

    feat(UsuarioRoutes): add getSolicitudes route

--- backend/src/routes/UsuarioRoutes.ts
+++ backend/src/routes/UsuarioRoutes.ts
@@ -4,6 +4,7 @@ import { UsuarioController } from "../controller/UsuarioController"
 const router = express.Router();
 
 router.get("/", UsuarioController.all);
+router.get("/:id/solicitudes", UsuarioController.getSolicitudes);
 router.get("/:id", UsuarioController.one);
 router.post("/", UsuarioController.save);
 router.delete("/:id", UsuarioController.remove);

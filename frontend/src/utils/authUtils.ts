6a5628d19f6c011e2edd29c53285edc2645426fb
Franquinios <francopietrantuono999@gmail.com>
2025-08-15 19:32:22 -0300

    feat(authUtils): delete existCookie

--- frontend/src/utils/authUtils.ts
+++ frontend/src/utils/authUtils.ts
@@ -1,14 +1,5 @@
-// Utilidad para obtener cookies
-export const getCookie = (name: string): string | undefined => {
-    if (typeof document === 'undefined') return; // Para SSR
-  
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    
-    if (parts.length === 2) return parts.pop()?.split(';').shift();
-};
-
 // Utilidad para eliminar cookies
 export const deleteCookie = (name: string) => {
+    console.log(document.cookie)
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${window.location.hostname};`;
 };

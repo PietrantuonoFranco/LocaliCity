ddc92cf7c6888db8a67ebcddc24344878d56a653
Franquinios <francopietrantuono999@gmail.com>
2025-08-16 08:31:35 -0300

    fix(pais.ts): add credentials to createPais

--- frontend/src/api/pais.ts
+++ frontend/src/api/pais.ts
@@ -9,8 +9,16 @@ export const getAllPaises = () => api.get<RespuestaPaises>(entity).then(({ data 
 
 export const getPaisById = (id: number) => api.get(`${entity}/${id}`).then(({ data }) => data);
 
-export const createPais = ({ ...pais }) => {
-    api.post(entity, pais).then(({ data }) => data);
+export const createPais = async (nombre: string): Promise<RespuestaPaises | null> => {
+  const response = await api.post<RespuestaPaises>(entity, { nombre }, {
+    withCredentials: true,
+    headers: { 'Content-Type': 'application/json' },
+  }).then(({ data }) => data);
+
+  console.log(response)
+  if (response) return response;
+
+  return null;
 }
 
 export const deletePais = (id: number) => api.delete(`${entity}/${id}`);

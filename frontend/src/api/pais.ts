0cace13c0723ec771898d33aea1988079af9db48
Franquinios <francopietrantuono999@gmail.com>
2025-08-17 00:01:28 -0300

    feat(pais.ts): modify createPais return to RespuestaPais

--- frontend/src/api/pais.ts
+++ frontend/src/api/pais.ts
@@ -1,6 +1,6 @@
 import api from "./api";
 
-import type { RespuestaPaises } from "src/interfaces/RespuestasInterfaces";
+import type { RespuestaPais, RespuestaPaises } from "src/interfaces/RespuestasInterfaces";
 
 
 const entity: string = "paises";
@@ -9,13 +9,12 @@ export const getAllPaises = () => api.get<RespuestaPaises>(entity).then(({ data 
 
 export const getPaisById = (id: number) => api.get(`${entity}/${id}`).then(({ data }) => data);
 
-export const createPais = async (nombre: string): Promise<RespuestaPaises | null> => {
-  const response = await api.post<RespuestaPaises>(entity, { nombre }, {
+export const createPais = async (nombre: string): Promise<RespuestaPais | null> => {
+  const response = await api.post<RespuestaPais>(entity, { nombre }, {
     withCredentials: true,
     headers: { 'Content-Type': 'application/json' },
   }).then(({ data }) => data);
 
-  console.log(response)
   if (response) return response;
 
   return null;

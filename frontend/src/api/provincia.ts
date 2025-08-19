676251bed157a22ff166ec899745d3ad6c8a6882
Franquinios <francopietrantuono999@gmail.com>
2025-08-19 20:42:00 -0300

    feat(provincia.ts): add 'updateProvincia' and add type to 'getProvinciaById'

--- frontend/src/api/provincia.ts
+++ frontend/src/api/provincia.ts
@@ -8,7 +8,7 @@ const entity: string = "provincias";
 
 export const getAllProvincias = () => api.get<RespuestaProvincias>(entity).then(({ data }) => data);
 
-export const getProvinciaById = (id: number) => api.get(`${entity}/${id}`).then(({ data }) => data);
+export const getProvinciaById = (id: number) => api.get<RespuestaProvincia>(`${entity}/${id}`).then(({ data }) => data);
 
 export const createProvincia = async (nombre: string, pais: Pais): Promise<RespuestaProvincia | null> => {
   const response = await api.post<RespuestaProvincia>(entity, { nombre, pais }, {
@@ -22,3 +22,14 @@ export const createProvincia = async (nombre: string, pais: Pais): Promise<Respu
 }
 
 export const deleteProvincia = (id: number) => api.delete(`${entity}/${id}`);
+
+export const updateProvincia = async (id: number, nombre: string | null = null, pais: Pais | null = null): Promise<RespuestaProvincia | null> => {
+  const response = await api.put<RespuestaProvincia>(`${entity}/${id}`, { nombre, pais }, {
+    withCredentials: true,
+    headers: { 'Content-Type': 'application/json' },
+  }).then(({ data }) => data);
+
+  if (response) return response;
+
+  return null;
+}

3fc69acf0b02e3e85c021a45e2f236423a11f2f1
Franquinios <francopietrantuono999@gmail.com>
2025-08-19 21:20:29 -0300

    feat(LocalidadController): add 'update' method and relation 'provincia' and 'provincia.pais' to 'one'

--- backend/src/controller/LocalidadController.ts
+++ backend/src/controller/LocalidadController.ts
@@ -29,7 +29,8 @@ export class LocalidadController {
       }
 
       const localidad = await localidadRepository.findOne({
-        where: { id }
+        where: { id },
+        relations: ['provincia', 'provincia.pais']
       });
 
       if (!localidad) {
@@ -78,6 +79,43 @@ export class LocalidadController {
 
       return response.status(204).json({ mensaje: "Localidad eliminada correctamente." });
     } catch {
+      return response.status(500).json({ error: "Se ha producido un error interno del servidor." });
+    }
+  }
+
+  static async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = parseInt(request.params.id);
+    
+      if (!id || id  <= 0) {
+        return response.status(400).json({ error: "No se ha proporcionado un ID de localidad válido."});
+      }
+    
+      const { nombre, provincia } = request.body;
+    
+      if (!nombre && !provincia) {
+          return response.status(400).json({ 
+          error: "Debe proporcionar un nombre o un provincia para actualizar." 
+        });
+      }
+    
+      const localidad = await localidadRepository.findOneBy({ id });
+    
+      if (!localidad) {
+        return response.status(404).json({ error: "Localidad no encontrada." });
+      }
+    
+      if (nombre !== null) localidad.nombre = nombre;
+      if (provincia !== null) localidad.provincia = provincia;
+  
+      await localidadRepository.save(localidad);
+    
+      return response.status(200).json({ 
+        mensaje: "Localidad actualizada correctamente.",
+        localidad: localidad
+      });
+    } catch (error) {
+      console.log(error);
       return response.status(500).json({ error: "Se ha producido un error interno del servidor." });
     }
   }

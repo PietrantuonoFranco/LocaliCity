57e4cc90c011760649c09e14cc7ce3b6de1d95ea
Franquinios <francopietrantuono999@gmail.com>
2025-08-21 06:32:16 -0300

    feat(usuario.ts): add 'updateUsuario' and add type to 'getUsuarioById'

--- frontend/src/api/usuario.ts
+++ frontend/src/api/usuario.ts
@@ -8,7 +8,7 @@ const entity: string = "usuarios";
 
 export const getAllUsuarios = () => api.get<RespuestaUsuarios>(entity).then(({ data }) => data);
 
-export const getUsuarioById = (id: number) => api.get(`${entity}/${id}`).then(({ data }) => data);
+export const getUsuarioById = (id: number) => api.get<RespuestaUsuario>(`${entity}/${id}`).then(({ data }) => data);
 
 export const createUsuario = async (email: string, nombre: string, apellido: string, contrasenia: string, rol: Rol): Promise<RespuestaUsuario | null > => {
     const response = await api.post<RespuestaUsuario>(entity, {email, nombre, apellido, contrasenia, rol}).then(({ data }) => data);
@@ -19,3 +19,9 @@ export const createUsuario = async (email: string, nombre: string, apellido: str
 export const deleteUsuario = (id: number) => api.delete(`${entity}/${id}`);
 
 export const getSolicitudesOfUser = (id: number) => api.get<RespuestaSolicitudes>(`${entity}/${id}/solicitudes`).then(({ data }) => data);
+
+export const updateUsuario = async (id: number, email: string | null = null, nombre: string | null = null, apellido: string | null = null, contrasenia: string | null = null, rol: Rol | null = null): Promise<RespuestaUsuario | null > => {
+    const response = await api.put<RespuestaUsuario>(`${entity}/${id}`, {email, nombre, apellido, contrasenia, rol}).then(({ data }) => data);
+
+    return response ? response : null;
+}

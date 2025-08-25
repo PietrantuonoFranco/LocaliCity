9408e076533d73c216454eecace1033c97dd3d8c
Franquinios <francopietrantuono999@gmail.com>
2025-08-25 00:45:14 -0300

    feat(SearchBar): responsive design and placeholder depending on 'type'

--- frontend/src/components/react/SearchBar.tsx
+++ frontend/src/components/react/SearchBar.tsx
@@ -111,7 +111,7 @@ export default function SearchBar({ type, elements, setElements }: Parametres) {
   }, []);
 
   return (
-    <div className="w-76 relative pb-[3px] focus-within:gradient-border">
+    <div className="w-full xl:w-72 relative pb-[3px] focus-within:gradient-border">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="w-6 h-6 absolute top-[45%] -translate-y-1/2 left-2.5 md:left-3 z-10"
@@ -133,7 +133,21 @@ export default function SearchBar({ type, elements, setElements }: Parametres) {
         onChange={handleChange}
         onKeyDown={handleKeyDown}
         className="pl-10.5 md:pl-12 pr-4 md:text-base input"
-        placeholder="Buscar paises, provincias, localidades..."
+        placeholder={`Buscar ${
+          type === "paises" ? "país"
+        : 
+          type === "provincias" ? "provincia, país"
+        : 
+          type === "localidades" ? "localidad, provincia, país"
+        : 
+          type === "solicitudes" ? "email, país, provincia, localidad"
+        : 
+          type === "usuarios" ? "email, nombre, apellido"
+        :
+          type === "usuario-solicitudes" ? "país, provincia, localidad"
+        : 
+          "email, país, provincia, localidad"
+        }...`}
       />
     </div>
   );

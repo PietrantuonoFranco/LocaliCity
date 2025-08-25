060fb88f08a9d95a7426a1f3196f22f94f9ab014
Franquinios <francopietrantuono999@gmail.com>
2025-08-25 04:03:13 -0300

    feat(CuentaPage): add responsive design

--- frontend/src/components/react/cuenta/CuentaPage.tsx
+++ frontend/src/components/react/cuenta/CuentaPage.tsx
@@ -20,7 +20,8 @@ import type Usuario from "src/interfaces/entities/UsuarioInterface";
 export default function CuentaPage() {
   const [option,setOption] = useState("mi-cuenta");
   const [user,setUser] = useState<Usuario | null>(null);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const [showAside, setShowAside] = useState(false)
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -53,6 +54,7 @@ export default function CuentaPage() {
     }
   }
 
+
   if (isLoading) {
     return (
       <div className="w-full h-100 flex justify-center items-center text-white/75">
@@ -71,14 +73,14 @@ export default function CuentaPage() {
       {!user && (
         <div className="mt-16 flex justify-center items-center">
           <div className="px-10 py-8 flex justify-center items-center rounded-xl bg-white/50 shadow-lg">
-            <p className="text-xl">Debes <a href="iniciar-sesion" className="font-semibold text-blue-600 underline">iniciar sesión</a> para ver este contenido</p>
+            <p className="text-lg">Debes <a href="iniciar-sesion" className="font-semibold text-blue-600 underline">iniciar sesión</a> para ver este contenido</p>
           </div>
         </div>
       )}
 
       {user && (
-        <div className="min-h-full w-full">
-          <aside className="fixed top-16 inset-y-0 left-0 w-70 bg-linear-to-b from-sky-100 via-sky-100 via-sky-100 via-white/85 via-white/70 to-white/65 shadow-lg ">
+        <div className="w-full">
+          <aside className={`${showAside ? "" : "hidden"} absolute md:fixed z-500 top-16 inset-y-0 left-0 w-70 bg-linear-to-b from-sky-100 via-sky-100 via-sky-100 via-white/85 via-white/70 to-white/65 shadow-lg`}>
             <div className="p-4  bg-linear-to-b from-sky-400 via-sky-300 to-sky-100 flex flex-col items-center space-y-2 mb-2">
               <div className="w-full flex justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-14 h-14" viewBox="0 0 24 24">
@@ -105,7 +107,7 @@ export default function CuentaPage() {
             </div>
             
 
-            <div className="h-full w-full flex flex-col p-4 space-y-2">
+            <div className="w-full flex flex-col p-4 space-y-2">
               <button
                 type="button"
                 id="mi-cuenta"
@@ -207,7 +209,20 @@ export default function CuentaPage() {
             </div>
           </aside>
 
-          <main className="max-h-screen w-full pl-79 p-8">
+          <div className={`${showAside ? "left-65.25" : "left-2"} z-510 fixed top-1/2 -translate-y-1/2`}>
+            <button type="button" onClick={() => setShowAside(!showAside)} className="p-1 rounded-full border-2 border-gray-500 text-gray-500 flex justify-center items-center bg-white/95 shadow-md hover:bg-gray-200 hover:shadow-lg transition-all duration-300">
+              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} className={`${showAside ? "" : "rotate-180"}`} viewBox="0 0 24 24">
+                <g fill="none">
+                  <path strokeWidth={0.5} d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path>
+                  <path strokeWidth={0.5} fill="currentColor" d="M3.283 10.94a1.5 1.5 0 0 0 0 2.12l5.656 5.658a1.5 1.5 0 1 0 2.122-2.122L7.965 13.5H19.5a1.5 1.5 0 0 0 0-3H7.965l3.096-3.096a1.5 1.5 0 1 0-2.122-2.121z"></path>
+                </g>
+              </svg>
+            </button>
+          </div>
+
+          <main className={`max-h-screen w-full p-4 md:p-8 transition-all duration-300 ${
+            showAside ? "md:pl-79" : ""
+          }`}>
             <div id="mi-cuenta-container" className="h-full w-full">
               {option === "mi-cuenta" && (
                 <MiCuenta/>

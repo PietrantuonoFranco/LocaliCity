983c22a5ad90ad9eeab2f46c963d35f08a460e8f
Franquinios <francopietrantuono999@gmail.com>
2025-08-25 03:59:39 -0300

    feat(Localidades): add responsive design

--- frontend/src/components/react/localidades/Localidades.tsx
+++ frontend/src/components/react/localidades/Localidades.tsx
@@ -173,10 +173,10 @@ export default function Localidades() {
   return (
     <>
       {localidades && (
-        <div className="w-full border-1 border-gray-200 shadow-md bg-white/50 rounded-2xl w-full py-10 px-12">
-          <div className="h-full flex flex-col">
-            <div className="w-full flex items-center justify-between">
-              <div className="flex items-center space-x-4">
+        <div className="w-full border-1 border-gray-200 shadow-md bg-white/50 rounded-2xl w-full p-6 md:py-10 md:px-12">
+        <div className="w-full h-full flex flex-col">
+          <div className="w-full flex flex-col xl:flex-row items-center xl:justify-between space-y-4 xl:space-y-0">
+            <div className="w-full flex items-center justify-between lg:justify-start lg:space-x-4">
                 <h2 className="text-3xl font-bold text-gray-800">Localidades</h2>
                 {!usuario && (
                   <a href="/iniciar-sesion" className="secondary-button w-10 h-10 flex items-center justify-center">
@@ -199,7 +199,7 @@ export default function Localidades() {
                 )}
               </div>
 
-              <div className="flex items-center space-x-4">
+              <div className="w-full flex items-center justify-end space-x-4">
                 <SearchBar
                   type="localidades"
                   elements={localidades}

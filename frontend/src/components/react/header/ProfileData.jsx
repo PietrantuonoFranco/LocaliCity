694d3bda4724d9bd65df8814ae0fee8d3a96cd82
Franquinios <francopietrantuono999@gmail.com>
2025-09-13 21:22:17 -0300

    fix(ProfileData.jsx): overflow while loading profile data

--- frontend/src/components/react/header/ProfileData.jsx
+++ frontend/src/components/react/header/ProfileData.jsx
@@ -46,8 +46,8 @@ export default function ProfileData () {
 
   if (loading) {
     return (
-      <div className="w-full h-100 flex justify-center items-center text-black/75">
-        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+      <div className="w-full h-full my-6 lg:my-0 flex justify-center lg:justify-end items-center text-black/75">
+        <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 24 24">
           <path fill="none" stroke="currentColor" strokeDasharray={16} strokeDashoffset={16} strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M12 3c4.97 0 9 4.03 9 9">
             <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="16;0"></animate>
             <animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"></animateTransform>
